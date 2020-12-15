1b3927f598f21f01adb04cce589e923b86ff37fd
Imad EL Kholti <im.el-kholti@domain.com>
2020-12-15 18:34:29 +0100

    Updates

---
+++ precache-manifest.a3764ef3dcc7a63c4b158aa77b3b8a7f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "afb6c72bc26e7aa5f5f1",
+    "url": "/match/static/js/app.a747e23f.chunk.js"
+  },
+  {
+    "revision": "96715bf0a411c6dde945",
+    "url": "/match/static/js/runtime~app.e652ff3a.js"
+  },
+  {
+    "revision": "ac2012e29f478e8573dc",
+    "url": "/match/static/js/2.16948c3b.chunk.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/match/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "5bf04ab163c03baed680c095f0382608",
+    "url": "/match/static/media/ptch.a355ccf1.jpg"
+  },
+  {
+    "revision": "a9b030e29a35f523a137ee0175be419f",
+    "url": "/match/./fonts/DroidKufi-Regular.ttf"
+  },
+  {
+    "revision": "1b89eb34f74a02c0681727faadf48466",
+    "url": "/match/./fonts/DroidKufi-Bold.ttf"
+  },
+  {
+    "revision": "b3263095df30cb7db78c613e73f9499a",
+    "url": "/match/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "c0eaa4a331236fdbf363796c6012cd00",
+    "url": "/match/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/match/serve.json"
+  },
+  {
+    "revision": "62c83184f7a960473239dbaa6ccc5b30",
+    "url": "/match/expo-service-worker.js"
+  },
+  {
+    "revision": "f4da2636873d98fba2d3d3172da355c0",
+    "url": "/match/register-service-worker.js"
+  },
+  {
+    "revision": "d324ddc823eca6c86688f62576c81e76",
+    "url": "/match/favicon-16.png"
+  },
+  {
+    "revision": "e72daee58e37c99d3553059eda418aa8",
+    "url": "/match/favicon-32.png"
+  },
+  {
+    "revision": "50b1bd72d8b0c569ffcad83d622ec176",
+    "url": "/match/favicon.ico"
+  },
+  {
+    "revision": "e988c90dac743dc84a8c3f31cddd2c90",
+    "url": "/match/manifest.json"
+  }
+];
---
+++ register-service-worker.js
@@ -0,0 +1,14 @@
+/* eslint-env browser */
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker
+      .register('/match/expo-service-worker.js', { scope: '/match/' })
+      .then(function (info) {
+        // console.info('Registered service-worker', info);
+      })
+      .catch(function (error) {
+        console.info('Failed to register service-worker', error);
+      });
+  });
+}
