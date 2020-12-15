a0ebebd9ab7a5ae22175bb04baf66a4e9896bf69
Imad EL Kholti <im.el-kholti@domain.com>
2020-12-15 19:03:07 +0100

    Updates

--- register-service-worker.js
+++ register-service-worker.js
@@ -3,7 +3,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
     navigator.serviceWorker
-      .register('/match/expo-service-worker.js', { scope: '/match/' })
+      .register('/expo-service-worker.js', { scope: '/' })
       .then(function (info) {
         // console.info('Registered service-worker', info);
       })
