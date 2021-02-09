85ecdb3880d06d32a02aff73a024afa53ac2e776
Imad EL Kholti <im.el-kholti@domain.com>
2021-02-09 16:41:22 +0100

    Updates

--- register-service-worker.js
+++ register-service-worker.js
@@ -1,1 +1,7 @@
-"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/match/expo-service-worker.js",{scope:"/match"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
+"serviceWorker"in navigator&&window.addEventListener("load",(function(){
+    navigator.serviceWorker.register("/match/expo-service-worker.js",{scope:"/"})
+        .then(reg => {
+            console.log('Registration succeeded. Scope is ' + reg.scope);
+        })
+        .catch((function(e){console.info("Failed to register service-worker",e)}))
+}));
