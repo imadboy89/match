e44d40a7e6d4750d7263606e3d6561b81c4c7734
Imad EL Kholti <im.el-kholti@domain.com>
2021-07-12 12:05:24 +0100

    Updates

---
+++ precache-manifest.d91c25eee7117edd9f41f02880cbbfcb.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "540ca527e1f86c49b3e5",
+    "url": "/static/js/app.d3709598.chunk.js"
+  },
+  {
+    "revision": "66090dc4495d4ce3dfb9",
+    "url": "/static/js/runtime~app.2e9f1821.js"
+  },
+  {
+    "revision": "d199af9763cc8cfef1a5",
+    "url": "/static/js/2.1b160cb0.chunk.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "5bf04ab163c03baed680c095f0382608",
+    "url": "/static/media/ptch.a355ccf1.jpg"
+  },
+  {
+    "revision": "a9b030e29a35f523a137ee0175be419f",
+    "url": "/./fonts/DroidKufi-Regular.ttf"
+  },
+  {
+    "revision": "1b89eb34f74a02c0681727faadf48466",
+    "url": "/./fonts/DroidKufi-Bold.ttf"
+  },
+  {
+    "revision": "b3263095df30cb7db78c613e73f9499a",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "f11d1f98f4c05a9dc023bc5bd25d0eb7",
+    "url": "/404.html"
+  },
+  {
+    "revision": "db315164e5620f847ad79ca57b967b6d",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "57417ca62844a1cbb328e6c07b9a8d57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "4b5163239558311a45414e3418f2bfa4",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "d0b5a08f5133b5af78434989a3d386be",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "feb973a4b49ffea2edf7aa628d549876",
+    "url": "/static/js/2.1b160cb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffb82b76e33d67379fb924af14b176ca",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "8d379fe80d6b481d5dc416abd69cca55",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "3c6b4bd0f10d67852133e8ae1b60e2d3",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "088694f05db480e6f83f62608c450b54",
+    "url": "/manifest.json"
+  }
+];
