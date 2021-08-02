cf4b5b37b9b4805f227c5c6bd578caba64e0d330
Imad EL Kholti <im.el-kholti@domain.com>
2021-08-02 11:17:34 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.754821ad5b7f4f3672e0135efaf44100.js"
+  "/precache-manifest.cac2f6bc00b4c1deeaf63f7732ce2801.js"
 );
 
 workbox.skipWaiting();
