2c0d77708c943c5abdc418011299283df26f1f28
Imad EL Kholti <im.el-kholti@domain.com>
2021-08-02 11:17:40 +0100

    fix bugs and more ..DOALL

--- Pages/Movies.js
+++ Pages/Movies.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Picker, Modal, Button, TouchableOpacity, RefreshControl } from 'react-native';
+import { View, Picker, Modal, Alert , TextInput , RefreshControl } from 'react-native';
 import Constants from 'expo-constants';
 import ItemsList from '../components/list';
 import DateTimePicker from '@react-native-community/datetimepicker';
@@ -17,6 +17,7 @@ class MoviesScreen extends React.Component {
         genre:"",
         rate:0,
         sortby:"date_added",
+        search_qeury:"",
 
     };
     this.rates=[0,1,2,3,4,5,6,7,8,9];
@@ -43,14 +44,7 @@ class MoviesScreen extends React.Component {
   componentDidMount(){
     this._isMounted=true;
     getTheme("styles_news").then(theme=>this.setState({dynamic_style:theme}));
-    this.props.navigation.setOptions({title: "Movies",
-    "headerRight":()=>(
-          <IconButton 
-            name="refresh" size={this.state.dynamic_style.title.fontSize} style={this.state.dynamic_style.icons} onPress={()=>{
-            this.get_movies();
-          }}  />
-  )
-});
+    this.render_header();
   }
 
   refresh_list=()=>{
@@ -75,6 +69,8 @@ get_movies =(loading=true,keep_list=false)=>{
   options.genre          = this.state.genre;
   options.sort_by        = this.state.sortby;
   options.minimum_rating = this.state.rate;
+  options.query_term = this.state.search_qeury;
+  
   API_.get_yify_movies(options).then(data=>{
     if(this._isMounted){
       if(keep_list){
@@ -106,6 +102,24 @@ get_movies =(loading=true,keep_list=false)=>{
     this.state.sortby = itemValue;
     this.state.page=1;
     this.get_movies();
+  }
+  render_header(){
+    const iconsSize = this.state.dynamic_style && this.state.dynamic_style.title ? this.state.dynamic_style.title.fontSize : 15;
+    this.props.navigation.setOptions({
+      title:"Movies",
+      "headerRight":()=>(
+        <View style={{flexDirection:"row",margin:5,padding:5}}>
+            <TextInput 
+              style={{flex:1,backgroundColor:"black",color:"white",marginLeft:20,marginVertical:5,borderWidth:1,borderColor:"white",borderRadius:5}}
+              onChangeText={(val)=>{this.setState({search_qeury:val});this.render_header();}}
+              value={this.state.search_qeury}
+              />
+          <IconButton 
+            name="search" size={this.state.dynamic_style.title.fontSize} style={this.state.dynamic_style.icons} 
+            onPress={this.get_movies}  />
+        </View>
+      )
+    });
   }
   render() {
     const picker_style = {height:"90%",backgroundColor:"#2d3436",color:"#dfe6e9" ,borderColor:"white",borderWidth:1};
