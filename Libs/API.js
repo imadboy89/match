60f1c9ac3757ad62442af6b5d921ce3d2ebba80f
Imad EL Kholti <im.el-kholti@domain.com>
2022-04-13 11:32:12 +0000

    fix bugs and more ..DOALL

--- Libs/API.js
+++ Libs/API.js
@@ -13,7 +13,7 @@ class API {
     this.first_api_call_almtchapi = true;
     this.running_calls = [];
     this.server_url = "http://107.152.39.225:81/imad_404/";
-    this.server_url = "http://20.224.94.179/imad_404/";
+    this.server_url = "http://20.101.63.9/imad_404/";
     //this.server_url = "https://107.152.39.225/";
     this.yify_movies_url = "https://yts.mx/api/v2/";
     this.yify_subs_url = "https://yifysubtitles.org/";
@@ -689,7 +689,7 @@ class API {
     url = url+"?"+configs.body;
     this.token_tries-=1;
     if(!this.running_calls_check(url)){return [];}
-    return this.http(url, "POST", {}, this.headers, true,false,false).then(resJson=>{
+    return this.http(url, "POST", configs.body, this.headers, true,false,false).then(resJson=>{
       this.running_calls_remove(url);
       console.log(" token_tries -= 1", resJson);
       if(resJson && resJson["status"]== "true" && resJson["message"]){
--- Libs/updated.js
+++ Libs/updated.js
@@ -1,1 +1,1 @@
-﻿export default '2022-04-08 03:43:29';
+﻿export default '2022-04-13 11:32:11';
